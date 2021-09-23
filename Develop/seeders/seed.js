c51402470db06f9e66a743af43ace2767852bdfe
JNM87 <J.molina22587@gmail.com>
2021-09-23 15:17:45 -0400

    deploying 2

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -2,12 +2,11 @@ let mongoose = require("mongoose");
 let db = require("../models");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
+  process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  
   }
 );
 
